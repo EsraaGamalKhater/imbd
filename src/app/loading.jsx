f10c8b3086db4f191c09b5b3feccecca33617a4d
Your Name <your@email.com>
2024-11-22 19:03:38 +0200

    update

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from "next/image";
 
 export default function loading() {
   return (
     <div className='flex justify-center mt-16'>
-        <img src='spinner.svg'alt='loading'></img>
+        <Image  src='spinner.svg'alt='loading' />
         
     </div>
   )
--- src/component/NavbarItem.jsx
+++ src/component/NavbarItem.jsx
@@ -1,20 +1,28 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
 export default function NavbarItem({ title, param }) {
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavbarLink title={title} param={param} />
+    </Suspense>
+  );
+}
+
+function NavbarLink({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
   
-    return (
-        <div>
-            <Link 
-                className={`hover:text-amber-600 font-bold ${ genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-700 rounded-lg' : ''}`} 
-                href={`/?genre=${param}`} 
-                aria-current={genre === param ? 'page' : undefined}>
-                {title}
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <Link 
+        className={`hover:text-amber-600 font-bold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-700 rounded-lg' : ''}`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
 }
