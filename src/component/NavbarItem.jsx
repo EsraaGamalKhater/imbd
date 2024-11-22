49032d45a142744052efab939d8a88cf668bd6fe
Your Name <your@email.com>
2024-11-22 18:52:37 +0200

    update

--- src/component/NavbarItem.jsx
+++ src/component/NavbarItem.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
-export default function NavbarItem({ title, param })
-{
+export default function NavbarItem({ title, param }) {
     const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
-  return (
-    <div>
-     <Link className={`hover:text-amber-600 font-bold ${ genre === param? 'underline underline-offset-8 decoration-4 decoration-amber-700 rounded-lg' : ''}`}
-     href={`/?genre=${param}`}>{title}</Link>
-    </div>
-  ); 
+  
+    return (
+        <div>
+            <Link 
+                className={`hover:text-amber-600 font-bold ${ genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-700 rounded-lg' : ''}`} 
+                href={`/?genre=${param}`} 
+                aria-current={genre === param ? 'page' : undefined}>
+                {title}
+            </Link>
+        </div>
+    );
 }
